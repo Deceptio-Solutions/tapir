7a94f07634d1b6b2bb66662595ad751a0f1eefc9
jcran <jcran@0x0e.org>
2014-08-13 16:43:55 -0400

    production restart

--- script/restart.sh
+++ script/restart.sh
@@ -4,7 +4,11 @@ export RAILS_ENV=production
 
 # restart thin
 echo "killing thin appserver"
-for x  in `ps aux  |grep thin | cut -d ' '  -f 6`;do kill -9 $x;done
+#for x  in `ps aux  |grep thin | cut -d ' '  -f 6`;do kill -9 $x;done
+
+kill -9 `cat /home/jcran/tapir/tmp/pids/thin.0.pid`
+kill -9 `cat /home/jcran/tapir/tmp/pids/thin.1.pid`
+kill -9 `cat /home/jcran/tapir/tmp/pids/thin.2.pid`
 
 #application serveri
 echo "starting appserver"
@@ -12,8 +16,8 @@ bundle exec thin start -e production -s3 --socket /tmp/thin.sock
 
 # background task processingi
 echo "killing background workers"
-rvmsudo bundle exec god terminate
+RAILS_ENV=production rvmsudo bundle exec god terminate
 echo "starting background workers"
-rvmsudo bundle exec god -c god_config
+RAILS_ENV=production rvmsudo bundle exec god -c god_config
 
 echo "done"

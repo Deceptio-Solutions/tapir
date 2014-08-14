74b00386bedf3f67058a150ce0a49db4381cde48
jcran <jcran@0x0e.org>
2014-08-14 03:34:34 -0400

    restart script

--- script/restart.sh
+++ script/restart.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-TAPIR_ENV=development
+TAPIR_ENV=production
 
 # restart thin
 echo "killing thin appserver"
-#for x  in `ps aux  |grep thin | cut -d ' '  -f 6`;do kill -9 $x;done
-
-kill -9 `cat /home/jcran/tapir/tmp/pids/thin.0.pid`
-kill -9 `cat /home/jcran/tapir/tmp/pids/thin.1.pid`
-kill -9 `cat /home/jcran/tapir/tmp/pids/thin.2.pid`
+for x  in `pgrep -l -f thin| cut -d ' ' -f 1`;do kill -9 $x;done
 
 #application serveri
 echo "starting appserver"

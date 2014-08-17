97dbe88627fbab40b4cbdde71036ebdfa8bf4586
jcran <jcran@pentestify.com>
2014-08-17 01:34:43 -0700

    add a timeout to gather_ssl_certificate

--- lib/tapir/tasks/gather_ssl_certificate.rb
+++ lib/tapir/tasks/gather_ssl_certificate.rb
@@ -41,38 +41,42 @@ def run
   port = 443
 
   begin
-    # Create a socket and connect
-    tcp_client = TCPSocket.new hostname, port
-    ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client
-    
-    # Grab the cert
-    ssl_client.connect
+    Timeout.timeout(10) do
+      # Create a socket and connect
+      tcp_client = TCPSocket.new hostname, port
+      ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client
+      
+      # Grab the cert
+      ssl_client.connect
 
-    # Parse the cert
-    cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
+      # Parse the cert
+      cert = OpenSSL::X509::Certificate.new(ssl_client.peer_cert)
 
-    # Check the subjectAltName property, and if we have names, here, parse them.
-    cert.extensions.each do |ext|
-      if ext.oid =~ /subjectAltName/
+      # Check the subjectAltName property, and if we have names, here, parse them.
+      cert.extensions.each do |ext|
+        if ext.oid =~ /subjectAltName/
 
-        alt_names = ext.value.split(",").collect do |x|
-          x.gsub(/DNS:/,"").strip
+          alt_names = ext.value.split(",").collect do |x|
+            x.gsub(/DNS:/,"").strip
+          end
+
+          alt_names.each do |alt_name|
+            create_entity Entities::DnsRecord, { :name => alt_name }
+          end
+
         end
-
-        alt_names.each do |alt_name|
-          create_entity Entities::DnsRecord, { :name => alt_name }
-        end
-
       end
+
+      # Close the sockets
+      ssl_client.sysclose
+      tcp_client.close
+
+      # Create an SSL Certificate entity
+      create_entity Entities::SslCert, { :name => cert.subject,
+                                         :text => cert.to_text }
     end
-
-    # Close the sockets
-    ssl_client.sysclose
-    tcp_client.close
-
-    # Create an SSL Certificate entity
-    create_entity Entities::SslCert, { :name => cert.subject,
-                                       :text => cert.to_text }
+  rescue Timeout::Error
+    @task_logger.log "Timed out"
   rescue OpenSSL::SSL::SSLError => e
     @task_logger.error "Caught an error: #{e}"
   rescue Errno::ECONNRESET => e 

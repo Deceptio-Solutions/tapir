c1391a1af22e2d193d04611ff540a05dc60a4ae7
jcran <jcran@pentestify.com>
2014-12-09 13:50:54 -0800

    tweak webapp entity creation

--- lib/tapir/tasks/convert_netsvc_to_servers.rb
+++ lib/tapir/tasks/convert_netsvc_to_servers.rb
@@ -34,17 +34,17 @@ def run
   super
 
 # This method likely will not work anymore, as entities do not have hosts
-# tied directly to them. Expect we'll have to go through the parents for a 
+# tied directly to them. Expect we'll have to go through the parents for a
 # Host type, and then depending on the port number, choose the appropriate
 # type of server
 
 return @task_logger.error "No associated host :(" unless @entity.host
 
-server_types = [  
+server_types = [
   {:port_num => 21,   :proto => "tcp", :entity_type => Entities::FtpServer, :entity_name => "#{@entity.host.name}" },
   {:port_num => 22,   :proto => "tcp", :entity_type => Entities::SshServer, :entity_name => "#{@entity.host.name}" },
   {:port_num => 23,   :proto => "tcp", :entity_type => Entities::TelnetServer, :entity_name => "#{@entity.host.name}" },
-  {:port_num => 53,   :proto => "udp", :entity_type => Entities::DnsServer, :entity_name => "#{@entity.host.name}" }, 
+  {:port_num => 53,   :proto => "udp", :entity_type => Entities::DnsServer, :entity_name => "#{@entity.host.name}" },
   {:port_num => 80,   :proto => "tcp", :entity_type => Entities::WebApplication, :entity_name => "http://#{@entity.host.name}" },
   {:port_num => 443,  :proto => "tcp", :entity_type => Entities::WebApplication, :entity_name => "https://#{@entity.host.name}" },
   {:port_num => 8080, :proto => "tcp", :entity_type => Entities::WebApplication, :entity_name => "http://#{@entity.host.name}" },
@@ -53,7 +53,7 @@ server_types = [
 ]
 
 # for each of the types we know about above
-server_types.each do |s| 
+server_types.each do |s|
 
   # check to see if this server type matches
   if @entity.port_num == s[:port_num] and @entity.proto == s[:proto]
@@ -62,16 +62,14 @@ server_types.each do |s|
 
   # determine if this is an SSL application
   ssl = true if [443,8443].include? @entity.port_num
-  
+
   # construct uri
   protocol = ssl ? "https://" : "http://"
   uri = "#{protocol}#{@entity.host.name}:#{@entity.port_num}"
 
   # Create a web application entity based on the service
   create_entity(Entities::WebApplication, {
-    :name => uri,
-    :host => @entity.host,
-    :netsvc => @entity
+    :name => uri
   })
 
   # Resolve DNS records for this netsvc
@@ -89,14 +87,14 @@ server_types.each do |s|
     @task_logger.error "Hit exception: #{e}"
   end
 
-  # For each attached dns record, do the same
-  @entity.host.dns_records.each do |d|
-    uri = "#{protocol}#{d.name}:#{@entity.port_num}"
-    create_entity(Entities::WebApplication, {
-      :name => uri,
-      :host => @entity.host,
-      :netsvc => @entity
-    })
+  if @entity.host
+    # For each attached dns record, do the same
+    @entity.host.dns_records.each do |d|
+      uri = "#{protocol}#{d.name}:#{@entity.port_num}"
+      create_entity(Entities::WebApplication, {
+        :name => uri
+      })
+    end
   end
 end
 end
